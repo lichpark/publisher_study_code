57b90677d4e43e6a1f04aba44cd8451f83aeb3c6
lichpark <lichpark0@hanmail.net>
2024-11-15 17:44:05 +0900

    Delete React/react_theory/src directory

--- React/react_theory/src/HappyButton.tsx
+++
@@ -1,4 +0,0 @@
-const HappyButton = () => {
-  return <button>해피버튼</button>;
-};
-export default HappyButton;
--- React/react_theory/src/TextCar.tsx
+++
@@ -1,8 +0,0 @@
-const TextCar = () => {
-  const bottom__div: React.CSSProperties = {
-    fontSize: "25px",
-  };
-  return <div style={bottom__div}>"12가3546"</div>;
-};
-
-export default TextCar;
